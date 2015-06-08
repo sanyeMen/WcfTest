652ee4788471606f1e0632f3ab68f6d7e0f99b16
sanyeMen <liu610721@gmail.com>
2015-06-08 17:44:22 +0800

    娣诲姞prism

---
+++ WpfWorkTest/viewModel/ViewTest.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Interactivity.InteractionRequest;
+using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfWorkTest.viewModel
+{
+    public class ViewTest : BindableBase, IInteractionRequestAware
+    {
+        public string PageCount { get; set; }
+
+        private readonly DelegateCommand btCommand;
+
+        public ViewTest()
+        {
+            btCommand = new DelegateCommand(new Action(Do));
+        }
+
+        public ICommand ButtonCommand
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    MessageBox.Show("Button");
+                });
+            }
+        }
+
+        public void Do()
+        {
+            string s = "";
+        }
+
+        Action IInteractionRequestAware.FinishInteraction
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        INotification IInteractionRequestAware.Notification
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
